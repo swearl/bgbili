2e2b525ef89c3e65e32c530d4407a1e614bd8fba
靓华 <swearl@163.com>
2021-06-18 14:18:39 +0800

    使用JsonBird解决跨域问题

--- README.md
+++ README.md
@@ -1,23 +1,35 @@
 # bgbili
 
-把B站视频做成网页背景视频, [Demo](https://swearl.github.io/bgbili/)
+把 B 站视频做成网页背景视频, [Demo](https://swearl.github.io/bgbili/)
 
 ## 用法
 
 ### 加载 bgbili.css, jquery, bgbili.js
+
 ```html
-<link rel="stylesheet" href="./src/css/bgbili.css">
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="./src/css/bgbili.css" />
+<script
+  src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"
+></script>
 <script src="./src/js/bgbili.js"></script>
 ```
-### 加入一个div
+
+### 加入一个 div
+
 ```html
 <div id="bgbili"></div>
 ```
 
 ### 生成背景视频
+
 ```js
-$(function() {
-	$("#bgbili").bgbili({aid: 950945});
-})
+$(function () {
+  $("#bgbili").bgbili({ bvid: "BV11x411c711" });
+});
 ```
+
+## 感谢
+
+[JsonBird](https://github.com/xCss/JsonBird)
--- docs/js/bgbili.js
+++ docs/js/bgbili.js
@@ -1,57 +1,111 @@
-(function($) {
-	$.fn.bgbili = function(options) {
-		var settings = $.extend({
-			aid: 314,
-			page: 1,
-			ratio: 16 / 9
-		}, options);
+(function ($) {
+  $.fn.bgbili = function (options) {
+    var settings = $.extend(
+      {
+        bvid: "BV1xx411c7XW",
+        p: 1,
+        ratio: 16 / 9,
+      },
+      options
+    );
 
-		const self = $(this);
-		self.addClass("bgbili-player").after("<div class='bgbili-mask'></div>");
-		checkMeta();
+    const self = $(this);
+    self.addClass("bgbili-player").after("<div class='bgbili-mask'></div>");
+    checkMeta();
 
-		const url = `https://api.bilibili.com/playurl?aid=${settings.aid}&page=${settings.page}&platform=html5&vtype=mp4&type=jsonp`;
-		$.ajax({
-			url,
-			dataType: "jsonp",
-			success: res => {
-				console.log(res);
-				const video = `<video src='${res.durl[0].url.replace("http://", "https://")}' poster='${res.img.replace("http://", "https://")}' autoplay loop muted></video>`;
-				self.append(video);
-				alignVideo();
-			}
-		});
+    getCid(settings.bvid, settings.p).then((cid) => {
+      getVideo(settings.bvid, cid).then((res) => {
+        const video = `<video src='${res.durl[0].url.replace(
+          "http://",
+          "https://"
+        )}' autoplay loop muted></video>`;
+        self.append(video);
+        alignVideo();
+      });
+    });
 
-		$(window).on("resize", function() {
-			alignVideo();
-		});
+    // const url = `https://api.bilibili.com/playurl?aid=${settings.aid}&page=${settings.page}&platform=html5&vtype=mp4&type=jsonp`;
+    // $.ajax({
+    //   url,
+    //   dataType: "jsonp",
+    //   success: (res) => {
+    //     console.log(res);
+    //     const video = `<video src='${res.durl[0].url.replace(
+    //       "http://",
+    //       "https://"
+    //     )}' poster='${res.img.replace(
+    //       "http://",
+    //       "https://"
+    //     )}' autoplay loop muted></video>`;
+    //     self.append(video);
+    //     alignVideo();
+    //   },
+    // });
 
-		function alignVideo() {
-			const width = $(document).width(), height = $(document).height();
-			var videoWidth = 0, videoHeight = 0, videoLeft = 0, videoTop = 0;
-			if(width / height > settings.ratio) {
-				videoWidth = width;
-				videoHeight = videoWidth / settings.ratio;
-				videoTop = 0 - (videoHeight - height) / 2;
-			} else {
-				videoHeight = height;
-				videoWidth = videoHeight * settings.ratio;
-				videoLeft = 0 - (videoWidth - width) / 2;
-			}
-			$(".bgbili-player video").css({
-				"width": videoWidth,
-				"height": videoHeight,
-				"left": videoLeft,
-				"top": videoTop
-			})
-		}
+    $(window).on("resize", function () {
+      alignVideo();
+    });
 
-		function checkMeta() {
-			if($("meta[name='referrer']").length) {
-				$("meta[name='referrer']").attr("content", "no-referrer");
-			} else {
-				$("head").append("<meta name='referrer' content='no-referrer'>")
-			}
-		}
-	}
+    function alignVideo() {
+      const width = $(document).width(),
+        height = $(document).height();
+      var videoWidth = 0,
+        videoHeight = 0,
+        videoLeft = 0,
+        videoTop = 0;
+      if (width / height > settings.ratio) {
+        videoWidth = width;
+        videoHeight = videoWidth / settings.ratio;
+        videoTop = 0 - (videoHeight - height) / 2;
+      } else {
+        videoHeight = height;
+        videoWidth = videoHeight * settings.ratio;
+        videoLeft = 0 - (videoWidth - width) / 2;
+      }
+      $(".bgbili-player video").css({
+        width: videoWidth,
+        height: videoHeight,
+        left: videoLeft,
+        top: videoTop,
+        objectFit: "cover",
+      });
+    }
+
+    function checkMeta() {
+      if ($("meta[name='referrer']").length) {
+        $("meta[name='referrer']").attr("content", "no-referrer");
+      } else {
+        $("head").append("<meta name='referrer' content='no-referrer'>");
+      }
+    }
+
+    function getCid(bvid, p = 1) {
+      const url = `https://bird.ioliu.cn/v1/?url=https://api.bilibili.com/x/web-interface/view?bvid=${bvid}`;
+      return new Promise((resolve) => {
+        $.ajax({
+          url,
+          type: "get",
+          dataType: "json",
+          success(res) {
+            const v = res.data.pages[p - 1];
+            resolve(v.cid);
+          },
+        });
+      });
+    }
+
+    function getVideo(bvid, cid) {
+      const url = `https://bird.ioliu.cn/v1/?url=https://api.bilibili.com/x/player/playurl?cid=${cid}&platform=html5&bvid=${bvid}`;
+      return new Promise((resolve) => {
+        $.ajax({
+          url,
+          type: "get",
+          dataType: "json",
+          success(res) {
+            resolve(res.data);
+          },
+        });
+      });
+    }
+  };
 })(jQuery);
